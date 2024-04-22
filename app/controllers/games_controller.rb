aa700058f6b7765d687227f1f0469096a3f6556f
Soraya Dos Santos <dossantos.soraya@gmail.com>
2024-04-22 16:51:13 +0200

    Longest Word Game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { (65 + rand(26)).chr }.to_a
+  end
+
+  def included?
+    params[:score].chars.all? { |letter| params[:score].count(letter) <= params[:letters].count(letter) }
+  end
+
+  def score
+    if included?
+      if english_word?(params[:score])
+        @score = true
+        return @score
+      end
+    end
+  end
+
+  def english_word?(word)
+    response = URI.parse("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
